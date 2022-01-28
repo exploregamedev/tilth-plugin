d339186ab7d969e2fef88c774fbf8251912d94c5
samkeen <sam.sjk@gmail.com>
2022-01-28 08:14:53 -0800

    now init a starter project if not one exists

--- addons/tilth_project_tracker/plugin.gd
+++ addons/tilth_project_tracker/plugin.gd
@@ -1,11 +1,9 @@
 tool
 extends EditorPlugin
 
-#const MainPanel = preload("res://addons/tilth_project_tracker/main_scene.tscn")
 const ProjectBoard = preload("res://addons/tilth_project_tracker/tilth/scenes/project/project_board.tscn")
-#const ProjectBoard = preload("res://addons/tilth_project_tracker/tilth/scenes/project/projects_list.tscn")
 
-#var main_panel_instance: Control
+
 var project_board_instance: Control
 
 
@@ -22,7 +20,14 @@ func _enter_tree() -> void:
 	var project_repo = ResourceRepository.new()
 	project_repo.init("Project", AppSettings.resource_store_path)
 	var projects = project_repo.load_all_resources(true) # skip cache
-	project_board_instance.set_project(projects[0])
+	if not projects:
+		print("Creating initial Project")
+		var init_project = Project.new()
+		init_project.name = "My Project"
+		init_project.description = "The project description"
+		project_board_instance.set_project(init_project)
+	else:
+		project_board_instance.set_project(projects[0])
 	# Add the main panel to the editor's main viewport.
 	get_editor_interface().get_editor_viewport().add_child(project_board_instance)
 	# Hide the main panel. Very much required.
@@ -59,6 +64,3 @@ func auto_loads(add_or_remove: String) -> void:
 	else:
 		for autoload_name in Singletons:
 			remove_autoload_singleton(autoload_name)
-
-
-
